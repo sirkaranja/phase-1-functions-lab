5a23588bdfeed091abe50d8a25cc20edf36574c0
Daniel Karanja <sirkaranja12@gmail.com>
2023-03-21 23:06:10 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+// Code your solution in this file!
+function distanceFromHqInBlocks(streetblocks) {
+ distanceFromHqInBlocks= Math.abs(streetblocks-42);
+ return distanceFromHqInBlocks;
+}
+function distanceFromHqInFeet(streetblocks){
+  distanceFromHqInFeet=distanceFromHqInBlocks *264;
+  return distanceFromHqInFeet;
+}
+function distanceTravelledInFeet(start,destination){
+  distanceTravelledInFeet= Math.abs(destination-start) * 264;
+  return distanceTravelledInFeet;
+}
+function calculatesFarePrice(start,destination){
+  distanceTravelledInFeet= Math.abs(destination-start)*264;
+  if (distanceTravelledInFeet <= 400) {
+      calculatesFarePrice = 0;
+      return calculatesFarePrice;
+  } else if (distanceTravelledInFeet > 400 && distanceTravelledInFeet<2000) {
+      calculatesFarePrice  = Maths.abs(distanceTravelledInFeet - 400) * 2;
+      return calculatesFarePrice;
+  }else if (distanceTravelledInFeet > 2000 && distanceFromHqInFeet < 2500) {
+  
+     return calculatesFarePrice = 25;
+  } else {
+      return calculatesFarePrice = "cannot travel that far";
+  }
+}
